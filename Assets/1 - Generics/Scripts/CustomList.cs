9b982e91412d580d0d56a359fc2cf4c5a98a7d06
Alexander Zhang <rich_zhanga@outlook.com>
2017-07-24 12:53:15 +1000

    Add Function

---
+++ Assets/1 - Generics/Scripts/CustomList.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Generics
+{
+    [System.Serializable]
+    public class CustomList<T>
+    {
+        public T[] list;
+
+        public int capacity { get; }
+        public int amount { get; private set; }
+        public CustomList () { }
+        // gameObjects[0]
+        public T this[int index]
+        {
+            get
+            {
+                return list[index];
+            }
+            set
+            {
+                list[index] = value;
+            }
+        }
+
+        public void Add(T item)
+        {
+            // Create a new array of amount + 1
+            T[] cache = new T[amount + 1];
+            // Check if list has been initialized
+            if (list != null)
+            {
+                // Copy all existing items to new array
+                for (int i = 0; i < list.Length; i++)
+                {
+                    cache[i] = list[i];
+                }
+            }
+            // Place new item at end index
+            cache[amount] = item;
+            // Replace old array with new array
+            list = cache;
+            // Increment amount
+            amount++;          
+        }
+    }
+}
---
+++ Assets/1 - Generics/Scripts/GenericsTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Generics
+{
+
+    public class GenericsTest : MonoBehaviour
+    {
+        public CustomList<GameObject> gameObjects;
+        // Use this for initialization
+        void Start()
+        {
+            gameObjects = new CustomList<GameObject>();
+            gameObjects.Add(new GameObject());
+            gameObjects.Add(new GameObject());
+            gameObjects.Add(new GameObject());
+
+            GameObject firstObject = gameObjects[0];
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
