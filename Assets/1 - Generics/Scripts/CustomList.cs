c09d372e23475a683005430ae0c1cb11ac837323
Alex Zhang <rich_zhanga@outlook.com>
2017-07-30 14:16:50 +1000

    remove list

--- Assets/1 - Generics/Scripts/CustomList.cs
+++ Assets/1 - Generics/Scripts/CustomList.cs
@@ -9,9 +9,9 @@ namespace Generics
     {
         public T[] list;
 
-        public int capacity { get; }
+        //public int capacity { get; }
         public int amount { get; private set; }
-        public CustomList () { }
+        public CustomList() { }
         // gameObjects[0]
         public T this[int index]
         {
@@ -43,7 +43,56 @@ namespace Generics
             // Replace old array with new array
             list = cache;
             // Increment amount
-            amount++;          
+            amount++;
+        }
+    }
+
+    public class CarBrands<C>
+    {
+        // Getting and setting the stats for the items
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Cost { get; set; }
+        public string Type { get; set; }
+
+        public void CarDealership()
+        {
+            // Creating Items for each of the 2 lists
+            CarBrands<GameObject> car1 = new CarBrands<GameObject>()
+            {
+                Brand = "Ferrari",
+                Model = "458Italia",
+                Cost = 600000,
+                Type = "ExoticCar"
+            };
+            CarBrands<GameObject> car2 = new CarBrands<GameObject>()
+            {
+                Brand = "Lamborghini",
+                Model = "Murcielago",
+                Cost = 450000,
+                Type = "ExoticCar"
+            };
+            CarBrands<GameObject> car3 = new CarBrands<GameObject>()
+            {
+                Brand = "Ford",
+                Model = "GT",
+                Cost = 397500,
+                Type = "ExoticCar"
+            };
+            CarBrands<GameObject> car4 = new CarBrands<GameObject>()
+            {
+                Brand = "Bugatti",
+                Model = "Veyron",
+                Cost = 1500000,
+                Type = "SuperCar"
+            };
+            CarBrands<GameObject> car5 = new CarBrands<GameObject>()
+            {
+                Brand = "McLaren",
+                Model = "P1GTR",
+                Cost = 2000000,
+                Type = "SuperCar"
+            };
         }
     }
 }
--- Assets/1 - Generics/Scripts/GenericsTest.cs
+++ Assets/1 - Generics/Scripts/GenericsTest.cs
@@ -8,15 +8,26 @@ namespace Generics
     public class GenericsTest : MonoBehaviour
     {
         public CustomList<GameObject> gameObjects;
+        public CarBrands<GameObject> cars;
+
         // Use this for initialization
         void Start()
         {
+            // Add
             gameObjects = new CustomList<GameObject>();
             gameObjects.Add(new GameObject());
             gameObjects.Add(new GameObject());
             gameObjects.Add(new GameObject());
 
             GameObject firstObject = gameObjects[0];
+
+            // Add Range
+            CarBrands<GameObject> carList1 = new CarBrands<GameObject>();
+            carList1.Add(car1);
+
+            CarBrands<GameObject> carList2 = new CarBrands<GameObject>();
+
+            CarBrands.RemoveAt(car1);
         }
 
         // Update is called once per frame
