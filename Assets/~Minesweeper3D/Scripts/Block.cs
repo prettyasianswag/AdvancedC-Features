60e091a355c51e61e39a886d8011e5c9ee24acc1
Alexander Zhang <rich_zhanga@outlook.com>
2017-08-28 11:59:37 +1000

    3d Minesweeper

---
+++ Assets/~Minesweeper3D/Scripts/Block.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minesweeper3D
+{
+    [RequireComponent(typeof(Renderer))]
+    public class Block : MonoBehaviour
+    {
+        [Header("Ints")]
+        // Score X, Y and Z coordinate in array for later use
+        public int x = 0;
+
+        public int y = 0;
+
+        public int z = 0;
+
+        [Header("Bools")]
+        // Is the block a mine?
+        public bool isMine = false;
+
+        // Has the block been revealed?
+        private bool isRevealed = false;
+
+        [Header("References")]
+        // Array for text colours
+        public Color[] textColors;
+
+        // Reference to the text element
+        public TextMesh textElement;
+
+        // Reference to the mine
+        public Transform mine;
+
+        // Reference to the renderer
+        private Renderer rend;
+
+        void Awake()
+        {
+            // Grab the reference to the renderer
+            rend = GetComponent<Renderer>();
+        }
+
+        void Start()
+        {
+            // Detach Text Element from block
+            textElement.transform.SetParent(null);
+
+            // Randomly decide if it's a mine or not
+            isMine = Random.value < 0.5f;
+        }
+
+
+        void Update()
+        {
+
+        }
+
+        // Determining which index of colours to use for which number we have
+        void UpdateText (int adjacentMines)
+        {
+            // Are there adjacent mines?
+            if (adjacentMines > 0)
+            {
+                // Set text to amount of mines
+                textElement.text = adjacentMines.ToString();
+
+                // Check if adjacentMines are within textColour's array
+                if (adjacentMines >= 0 && adjacentMines < textColors.Length)
+                {
+                    // Set text color to whatever was preset
+                    textElement.color = textColors[adjacentMines];
+                }
+            }
+        }
+
+        public void Reveal (int adjacentMines)
+        {
+            // Flags the block as being revealed
+            isRevealed = true;
+            
+            // Checks if block is a mine
+            if (isMine)
+            {
+                // Activate the references mine
+                mine.gameObject.SetActive(true);
+
+                // Detach mine from children
+                mine.SetParent(null);
+            }
+            else
+            {
+                // Updates the text to display adjacentMines
+                UpdateText(adjacentMines);
+            }
+            // Deactivates the block
+            gameObject.SetActive(false);
+        }
+
+
+    }
+}
+
---
+++ Assets/~Minesweeper3D/Scripts/Grid.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minesweeper3D
+{
+    public class Grid : MonoBehaviour
+    {
+        public GameObject blockPrefab;
+
+        // The grid's dimensions
+        public int width = 10;
+        public int height = 10;
+        public int depth = 10;
+
+        // How much spacing between each Block
+        public float spacing = 1.2f;
+
+        // Multi-Dimensional Array storing the blocks (in this case 3D)
+        private Block[,,] blocks;
+
+        void Start()
+        {
+            // Generate blocks on startup
+            GenerateBlocks();
+        }
+
+        void Update()
+        {
+
+        }
+
+        // Spawns a block at position and returns the block component
+        Block SpawnBlock(Vector3 pos)
+        {
+            // Instantiate the clone
+            GameObject clone = Instantiate(blockPrefab);
+
+            // Setting the position of the clone
+            clone.transform.position = pos;
+
+            // Get Block Component
+            Block currentBlock = clone.GetComponent<Block>();
+
+            // Return
+            return currentBlock;
+        }
+
+        // Spawns block in a grid-like fashion
+        void GenerateBlocks()
+        {
+            // Create 3D array to store all the blocks
+            blocks = new Block[width, height, depth];
+
+            // Loop through the X, Y, Z axis of the 3D array
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    for (int z = 0; z < depth; z++)
+                    {
+                        // Calculate half size using arrray dimensions
+                        Vector3 halfSize = new Vector3(width / 2, height / 2, depth / 2);
+
+                        // Make sure to offset by half (so that elements are centered)
+                        halfSize -= new Vector3(0.5f, 0.5f, 0.5f);
+
+                        // Create position for element to pivot around Grid zero
+                        Vector3 pos = new Vector3(x - halfSize.x, y - halfSize.y, z - halfSize.z);
+
+                        // Apply Spacing (pos = pos * spacing)
+                        pos *= spacing;
+
+                        // Spawn the block at that position
+                        Block block = SpawnBlock(pos);
+
+                        // Attach block to grid as a child
+                        block.transform.SetParent(transform);
+
+                        // Store array coordinate inside the block itself
+                        block.x = x;
+                        block.y = y;
+                        block.z = z;
+
+                        // Store block in the array at coordinates
+                        blocks[x, y, z] = block;
+                    }
+                }
+            }
+        }
+    }
+}
