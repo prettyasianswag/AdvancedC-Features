c5919ec68929aabe68719c88f0f92c5ae18e467a
Alexander Zhang <rich_zhanga@outlook.com>
2017-10-16 09:16:46 +1100

    networking

---
+++ Assets/5 - Networking/Scripts/NetworkUser.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Networking;
+
+namespace Networking
+{
+    [RequireComponent(typeof(Player))]
+    public class NetworkUser : NetworkBehaviour
+    {
+        public Camera cam;
+        public AudioListener aListener;
+        private Player player;
+
+        // Use this for initialization
+        void Start()
+        {
+            player = GetComponent<Player>();
+            if (!isLocalPlayer)
+            {
+                cam.enabled = false;
+                aListener.enabled = false;
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Check if current client has authority over this player
+            if (isLocalPlayer)
+            {
+                float h = Input.GetAxis("Mouse X");
+                float v = Input.GetAxis("Vertical");
+                float mh = Input.GetAxis("Horizontal");
+                player.Move(h, v, mh);
+
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    player.Jump();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/5 - Networking/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Networking
+{
+    public class Player : MonoBehaviour
+    {
+        public float movementSpeed = 10.0f;
+        public float rotationSpeed = 10.0f;
+        public float jumpHeight = 2.0f;
+        private bool isGrounded = false;
+        private Rigidbody rigid;
+
+        // Use this for initialization
+        void Start()
+        {
+            rigid = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void Move(float h, float v, float mh)
+        {
+            Vector3 position = rigid.position;
+            Quaternion rotation = rigid.rotation;
+
+            position += transform.forward * v * movementSpeed * Time.deltaTime;
+          
+            rotation *= Quaternion.AngleAxis(rotationSpeed * h, Vector3.up);
+
+            position += transform.right * mh * movementSpeed * Time.deltaTime;
+            
+            rigid.MovePosition(position);
+            rigid.MoveRotation(rotation);
+        }
+
+        public void Jump()
+        {
+            if (isGrounded)
+            {
+                rigid.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+                isGrounded = false;
+            }
+        }
+
+        void OnCollisionEnter (Collision col)
+        {
+            isGrounded = true;
+        }
+    }
+}
+
